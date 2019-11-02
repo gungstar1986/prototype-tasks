e5bf919a6df45d72959e93cd57924ac5629ed904
Denis <gungstar1986@gmail.com>
2019-11-02 18:58:10 +0200

    5 __proto__ methods

--- learnJs.js
+++ learnJs.js
@@ -1,4 +1,3 @@
-
 // ------------------------------- class method
 
 class Example {
@@ -24,6 +23,53 @@ user.__proto__ = Example.prototype;
 
 console.log(user.move()); // Юзер научился ходить
 
+// ------------------------------- class X Exteds class XX 
+
+class Example {
+  constructor(name, age) {
+    (this.name = name), (this.age = age);
+  }
+  move() {
+    return `${this.name} can walk`;
+  }
+}
+const manager = new Example("David", 25);
+
+class ExampleProto extends Example {
+  cantMove() {
+    `${this.name} can\`t walk`;
+  }
+}
+const user = new ExampleProto("John", 24);
+
+console.log(user.move()); // Юзер научился ходить
+
+// ---------------------------------- тоже самое через super()
+
+class Example {
+  constructor(name, age) {
+    (this.name = name), (this.age = age);
+  }
+  move() {
+    return `${this.name} can walk`;
+  }
+}
+const manager = new Example("David", 25);
+
+class ExampleProto extends Example {
+  constructor(name, age) { // наследываение одного объекта конструктора от другого
+    super(name);
+    this.age = age;
+  }
+  cantMove() {
+    return `${this.name} can\`t walk`;
+  }
+}
+const user = new ExampleProto("John", 24);
+
+console.log(user.move()); // Юзер научился ходить
+console.log(user.cantMove()); // Юзер разучился ходить
+
 // ------------------------------- object method
 
 const manager = {
@@ -34,7 +80,7 @@ const manager = {
 
 const user = {
   name: "Dan",
-  __proto__: manager 
+  __proto__: manager
 };
 
 console.log(user.isBoss()); // Юзер стал Боссом ))
@@ -42,19 +88,19 @@ console.log(user.boss);
 
 // -------------------------------- Object.setPrototypeOf method
 
-function Manager (name, age) {
-   this.name = name;
-   this.age = age;
-   this.walk = function() {
-     return `${this.name} can walk`
-   }
+function Manager(name, age) {
+  this.name = name;
+  this.age = age;
+  this.walk = function() {
+    return `${this.name} can walk`;
+  };
 }
-const manager = new Manager("Tima", "unknow ;-)")
+const manager = new Manager("Tima", "unknow ;-)");
 
 const user = {
   name: "Dan",
-  age: 33,
-}
+  age: 33
+};
 
-Object.setPrototypeOf(user, manager)
-console.log(user.walk())
+Object.setPrototypeOf(user, manager);
+console.log(user.walk());
