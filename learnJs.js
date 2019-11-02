1f89b06257bf7eec33dbb31d98b5dcf72e0f04e1
Denis <gungstar1986@gmail.com>
2019-11-02 08:12:38 +0200

    proto

--- learnJs.js
+++ learnJs.js
@@ -1,27 +1,28 @@
+
 // ------------------------------- class method
 
-// class Example {
-//   constructor(name, age) {
-//     (this.name = name), (this.age = age);
-//   }
-//   move() {
-//     return `${this.name} can walk`;
-//   }
-// }
-// const manager = new Example("David", 25);
+class Example {
+  constructor(name, age) {
+    (this.name = name), (this.age = age);
+  }
+  move() {
+    return `${this.name} can walk`;
+  }
+}
+const manager = new Example("David", 25);
 
-// class ExampleProto {
-//   constructor(name, age) {
-//     (this.name = name), (this.age = age);
-//   }
-//   cantMove() {
-//     `${this.name} can\`t walk`;
-//   }
-// }
-// const user = new ExampleProto("John", 24);
-// user.__proto__ = Example.prototype;
+class ExampleProto {
+  constructor(name, age) {
+    (this.name = name), (this.age = age);
+  }
+  cantMove() {
+    `${this.name} can\`t walk`;
+  }
+}
+const user = new ExampleProto("John", 24);
+user.__proto__ = Example.prototype;
 
-// console.log(user.move());
+console.log(user.move()); // Юзер научился ходить
 
 // ------------------------------- object method
 
@@ -33,10 +34,9 @@ const manager = {
 
 const user = {
   name: "Dan",
-  __proto__: manager
+  __proto__: manager 
 };
 
-
-// user.isBoss = null
-console.log(user.isBoss());
+console.log(user.isBoss()); // Юзер стал Боссом ))
 console.log(user.boss);
+
