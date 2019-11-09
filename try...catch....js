842f782e1de8b05d5ca5ae3a5c275b2d44fb1530
Denis <gungstar1986@gmail.com>
2019-11-09 21:40:15 +0200

    add new lessons

---
+++ try...catch....js
@@ -0,0 +1,25 @@
+const usersDataBase = new Map();
+
+function addUser(userName, userAge) {
+  try {
+    const user = {
+      name: userName,
+      age: userAge
+    };
+    if (usersDataBase.has(user.name)) throw new Error("Такой юзер существует!");
+    if (user.age < 6) throw new Error("Пользователь слишком молод!")
+    usersDataBase.set(user.name, user.age);
+  } catch (error) {
+    console.log(error) 
+  } finally {
+    
+  }
+}
+
+addUser("Денис", 33);
+addUser("Яна", 35);
+addUser("Денис", 18);
+addUser("Женя", 6);
+addUser("Андрей", 4);
+
+console.log(usersDataBase)
