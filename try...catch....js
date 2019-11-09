5b4b69a86fcfe888be9faadda61e1c9d6c1b6f28
Denis <gungstar1986@gmail.com>
2019-11-09 22:21:34 +0200

    errors solved

--- try...catch....js
+++ try...catch....js
@@ -7,19 +7,17 @@ function addUser(userName, userAge) {
       age: userAge
     };
     if (usersDataBase.has(user.name)) throw new Error("Такой юзер существует!");
-    if (user.age < 6) throw new Error("Пользователь слишком молод!")
+    if (user.age < 6) throw new Error("Пользователь слишком молод!");
     usersDataBase.set(user.name, user.age);
   } catch (error) {
-    console.log(error) 
-  } finally {
-    
+    console.log(error);
   }
 }
 
 addUser("Денис", 33);
-addUser("Яна", 35);
+addUser("Яна", 34);
 addUser("Денис", 18);
 addUser("Женя", 6);
 addUser("Андрей", 4);
 
-console.log(usersDataBase)
+console.log(usersDataBase);
